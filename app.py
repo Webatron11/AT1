0917f0f9532221432eb6b44047d0aa4c5466c1a4
Oscar Webb <oscarwebb162@protonmail.com>
2024-11-27 19:40:28 +1100

    IDK IDE stuff

--- app.py
+++ app.py
@@ -1,33 +1,95 @@
-from flask import render_template, Flask
+from flask import render_template, Flask, request, redirect
 from sqlite3 import connect
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    database = connect("./database.db")
+def sendDBQuery(query: str):
+    database = connect('./database.db')
+    cursor = database.cursor()
+    result = cursor.execute(query)
+    database.commit()
+    return result
 
-    # Select all rows - product id, product name, product supplier, cost and stock.
-    # stock should be sum(stock.stock), grouped by id.
-    # cost should be the most recent cost from stock.cost
 
-    query = '''SELECT products.id, products.name, products.supplier, s.cost, SUM(s.change) AS "stock" 
-                FROM products 
-                INNER JOIN main.stock s on products.id = s.id 
-                GROUP BY s.id;'''
-    rows = database.execute(query).fetchall()
-    database.close()
+@app.route('/', methods=['POST', 'GET'])
+def indexPage():
+    if request.method == 'POST':
+        if request.form.get('id') == 'stockUnder':
+            query = '''SELECT products.id, products.sku, products.name, products.supplier, s.cost, SUM(s.change) AS "stock", datetime((SELECT MIN(stock.date) from main.stock WHERE stock.id = s.id), 'unixepoch') AS "date" 
+                                FROM products 
+                                INNER JOIN main.stock s on products.id = s.id 
+                                WHERE (SELECT SUM(s.change) FROM products INNER JOIN main.stock s on products.id = s.id GROUP BY s.id) < 5
+                                GROUP BY s.id;'''
+        elif request.form.get('id') == 'dateBefore':
+            query = '''SELECT products.id, products.sku, products.name, products.supplier, s.cost, SUM(s.change) AS "stock", datetime((SELECT MIN(stock.date) from main.stock WHERE stock.id = s.id), 'unixepoch') AS "date" 
+                                            FROM products 
+                                            INNER JOIN main.stock s on products.id = s.id 
+                                            WHERE (SELECT s.date FROM products INNER JOIN main.stock s on products.id = s.id GROUP BY s.id) < 1688169600
+                                            GROUP BY s.id;'''
+        elif request.form.get('id') == 'normal':
+            query = '''SELECT products.id, products.sku, products.name, products.supplier, s.cost, SUM(s.change) AS "stock", datetime((SELECT MIN(stock.date) from main.stock WHERE stock.id = s.id), 'unixepoch') AS "date" 
+                                FROM products 
+                                INNER JOIN main.stock s on products.id = s.id 
+                                GROUP BY s.id;'''
+    else:
+        query = '''SELECT products.id, products.sku, products.name, products.supplier, s.cost, SUM(s.change) AS "stock", datetime((SELECT MIN(stock.date) from main.stock WHERE stock.id = s.id), 'unixepoch') AS "date" 
+                            FROM products 
+                            INNER JOIN main.stock s on products.id = s.id 
+                            GROUP BY s.id;'''
+    dbresult = sendDBQuery(query)
+    rows = dbresult.fetchall()
+    try:
+        max_stock = max([rows[i][4] for i in range(len(rows))])
+    except ValueError:
+        max_stock = 0
 
-    return render_template('index.html', items=rows)
+    return render_template('index.html', items=rows, max_stock=max_stock)
+
+
+@app.route('/commands/updateStock')
+def updateStockPage():
+    query = '''SELECT products.id, products.name FROM products;'''
+    dbresult = sendDBQuery(query)
+    rows = dbresult.fetchall()
+    items = [f"{i[1]} (ID: {i[0]})" for i in rows]
+
+    return render_template('./commands/updateStock.html', items=items)
 
 
 @app.route('/commands/addItem')
+def addItemPage():
+    return render_template("./commands/addItem.html")
+
+
+@app.route('/commands/deleteItem')
+def deleteItemPage():
+    query = '''SELECT products.id, products.name FROM products;'''
+    dbresult = sendDBQuery(query)
+    rows = dbresult.fetchall()
+    items = [f"{i[1]} (ID: {i[0]})" for i in rows]
+
+    return render_template("./commands/deleteItem.html", items=items)
+
+
+@app.route('/database/', methods=['POST'])
 def addItem():
+    post_data = request.form.to_dict()
+    query = ''''''
+    if request.form.get("id") == "addItem":
+        if post_data['stockCost']:
+            query = f'''INSERT INTO main.products (name, supplier, sku) VALUES('{post_data['productName']}', '{post_data['productSupplier']}', '{post_data['productSKU']}');
+                        INSERT INTO main.stock VALUES(last_insert_rowid(), '{post_data['stockCost']}', '{post_data['stockUpdate']}', now());'''
+        else:
+            query = f'''INSERT INTO main.products (name, supplier, sku) VALUES('{post_data['productName']}', '{post_data['productSupplier']}', '{post_data['productSKU']}');'''
+    elif request.form.get("id") == "updateStock":
+        query = f'''INSERT INTO main.stock (id, cost, change, date) VALUES('{post_data['item'].split("(")[1].removeprefix('ID: ').removesuffix(")")}', '{post_data['stockPrice']}', '{post_data['stockChange']}', unixepoch('{post_data['stockDate']}'));'''
+    elif request.form.get("id") == 'deleteItem':
+        query = f'''DELETE FROM main.products WHERE id = {post_data['item'].split("(")[1].removeprefix('ID: ').removesuffix(")")};'''
 
+    sendDBQuery(query)
 
-    database = connect("./database.db")
-
+    return redirect("/", 303)
 
 
 app.run()
